601beb0711c95619b9a5967019c0ccc7c9460937
Danielle Martinoli <daniechris5@gmail.com>
2019-05-01 23:14:42 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,20 +1,16 @@
 def speak_to_grandma(phrase)
   if phrase === "I LOVE YOU GRANDMA!"
-end
     return "I LOVE YOU TOO PUMPKIN!"
 
     elsif phrase === "Hi Nana, how are you".downcase
-end
-    return "HUH?! SPEAK UP, SONNY!"
+      return "HUH?! SPEAK UP, SONNY!"
 
     elsif phrase === "Hi!".downcase
-end
       return "HUH?! SPEAK UP, SONNY!"
 
     elsif phrase === "What did you eat today?".upcase
-end
       return "NO, NOT SINCE 1938!"
 
     else phrase === "What?".upcase
-end
       return "NO, NOT SINCE 1938!"
+end
